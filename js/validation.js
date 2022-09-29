b1974707be79ba71d7d1ae88a462f68ab3d6567c
N Schepisi <schepisidev@gmail.com>
2022-09-29 13:36:46 -0700

    fixed navbar hamburger click now working.

--- js/validation.js
+++ js/validation.js
@@ -47,6 +47,12 @@ validation
     });
     
     
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
     
     
     
